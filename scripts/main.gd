8aab900e911b5221314c2883366013d8f2ab21d1
TeoLpz <107282255+TeoLpz@users.noreply.github.com>
2024-11-15 23:08:44 -0600

    Main_menu

---
+++ scripts/main.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+
+func _ready():
+	$VBoxContainer/Start.grab_focus()
+
+func reset_focus():
+	$VBoxContainer/Start.grab_focus()
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://level_select.tscn") 
+
+func _on_option_pressed():
+	get_tree().change_scene_to_file("res://scene/settings.tscn") 
+
+func _on_quit_pressed():
+	get_tree().quit()
---
+++ scripts/settings.gd
@@ -0,0 +1,4 @@
+extends TabContainer
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://scene/Main.tscn") 
